b1c5e82a0f94f58216edd56d1fcaccae9526eef4
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-07 07:42:19 +0300

    initial lab code of bird game

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float ForceAmount = 100;
+    private Rigidbody2D rigidBody2D;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody2D.velocity = Vector2.zero;
+            rigidBody2D.AddForce(new Vector2(0, ForceAmount));
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Restart();
+    }
+
+    private void Restart()
+    {
+        Game.Instance.Restart();
+        rigidBody2D.velocity = Vector2.zero;
+        transform.position = new Vector3(transform.position.x, 0, 0);
+    }
+
+    // restart game when bird falls out of screen
+    // there is a function for this. 
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+
+    public static Game Instance;
+
+    public GameObject TreePrefab;
+    public float TreeDistance = 1f; 
+    public float Speed = 1f;
+	
+    private List<GameObject> _trees;
+
+    private bool _stop;
+
+	void Start () {
+        _stop = false;
+        Instance = this;
+        _trees = new List<GameObject>();
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject tree = GameObject.Instantiate(TreePrefab);
+            _trees.Add(tree);
+            tree.transform.position = new Vector3(TreeDistance * i, Random.Range(-2f, 2f), 0f);
+        }
+    }
+	
+	void Update () {
+        if (!_stop)
+        {
+            foreach (GameObject tree in _trees)
+            {
+                tree.transform.position -= new Vector3(Time.deltaTime * Speed, 0f, 0f);
+                if (tree.transform.position.x < -TreeDistance * (_trees.Count / 2f))
+                {
+                    tree.transform.position += new Vector3(TreeDistance * _trees.Count, 0f, 0f);
+                }
+            }
+        }
+	}
+
+    public void Restart()
+    {
+        foreach (GameObject tree in _trees)
+        {
+            GameObject.Destroy(tree);
+        }
+        Start();
+    }
+
+    public void StopGame()
+    {
+        _stop = true;
+    }
+}
