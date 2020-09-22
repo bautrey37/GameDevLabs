7da369f511950e5bcb7a93b6fe653c95e894eef6
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-23 00:36:31 +0300

    powerup life

---
+++ SpaceGame/Assets/Scripts/PowerupLife.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupLife : MonoBehaviour
+{
+    public Vector2 MovementVector;
+
+    private new SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (renderer.isVisible)
+        {
+            transform.position += (Vector3)MovementVector * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.Lives++;
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+}
