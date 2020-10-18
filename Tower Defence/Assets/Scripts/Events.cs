c2ae6bd250ce2662f59c9babf8cec43d6b45e231
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-10-18 17:21:31 +0300

    Add scenario controller script

--- Tower Defence/Assets/Scripts/Events.cs
+++ Tower Defence/Assets/Scripts/Events.cs
@@ -6,4 +6,16 @@ public static class Events
 {
     public static event Action<TowerData> OnTowerSelected;
     public static void SelectTower(TowerData data) => OnTowerSelected?.Invoke(data);
+
+    public static event Action<int> OnSetGold;
+    public static void SetGold(int value) => OnSetGold?.Invoke(value);
+
+    public static event Func<int> OnRequestGold;
+    public static int RequestGold() => OnRequestGold?.Invoke() ?? 0;
+
+    public static event Action<int> OnSetLives;
+    public static void SetLives(int value) => OnSetLives?.Invoke(value);
+
+    public static event Func<int> OnRequestLives;
+    public static int RequestLives() => OnRequestLives?.Invoke() ?? 0;
 }
---
+++ Tower Defence/Assets/Scripts/ScenarioController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioController : MonoBehaviour
+{
+    public int Gold = 40;
+    public int Lives = 3;
+
+    private void Awake()
+    {
+        Events.OnSetGold += OnSetGold;
+        Events.OnRequestGold += OnRequestGold;
+
+        Events.OnSetLives += OnSetLives;
+        Events.OnRequestLives += OnRequestLives;
+    }
+
+    public void Start()
+    {
+        Events.SetGold(Gold);
+
+        StartLevel();
+    }
+
+    private void OnDestroy()
+    {
+        Events.OnSetGold -= OnSetGold;
+        Events.OnRequestGold -= OnRequestGold;
+    }
+
+    private void StartLevel()
+    {
+        // add spawn delay
+        // spawn enemies
+    }
+
+    private void WinLevel()
+    {
+        // when X # of enemies are killed?
+    }
+
+    private void LoseLevel()
+    {
+        // when lives on castle goes to 0
+    }
+
+    private void OnSetGold(int amount)
+    {
+        Gold = amount;
+    }
+
+    private int OnRequestGold()
+    {
+        return Gold;
+    }
+
+    private void OnSetLives(int amount)
+    {
+        Lives = amount;
+    }
+
+    private int OnRequestLives()
+    {
+        return Lives;
+    }
+}
