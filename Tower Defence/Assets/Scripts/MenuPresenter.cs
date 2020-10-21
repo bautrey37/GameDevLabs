49e18436c1df9a27c9d3bebcbd5ee533d2b9a332
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-10-21 13:15:07 +0300

    update menu from data

---
+++ Tower Defence/Assets/Scripts/MenuPresenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuPresenter : MonoBehaviour
+{
+    public RectTransform PanelTransform;
+    public ScenarioPresenter ScenarioPresenterPrefab;
+    public Button ExitButton;
+
+    public List<ScenarioData> Scenarios;
+
+    private void Awake()
+    {
+        ExitButton.onClick.AddListener(OnExit);
+    }
+
+    private void Start()
+    {
+        foreach (ScenarioData data in Scenarios)
+        {
+            ScenarioPresenter presenter = GameObject.Instantiate(ScenarioPresenterPrefab, PanelTransform);
+            presenter.SetData(data);
+        }
+    }
+
+    private void OnExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Tower Defence/Assets/Scripts/ScenarioPresenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScenarioPresenter : MonoBehaviour
+{
+    public TextMeshProUGUI NameText;
+
+    private ScenarioData ScenarioData;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void SetData(ScenarioData data)
+    {
+        ScenarioData = data;
+        NameText.text = data.PresentedName;
+
+    }
+
+    private void OnClick()
+    {
+
+    }
+
+}
