ec85132505333f14a4407bd014b95ebccc3239b3
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-09 00:38:01 +0200

    finish lab 10 - sounds

---
+++ Sounds/Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public AudioClipGroup CoinDropAudio;
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            CoinDropAudio.Play();
+        }
+    }
+}
