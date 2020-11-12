391cb973f3476054e8bee5127f055eb844f30e40
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-12 23:50:32 +0200

    finish sound lab

--- Sounds/Assets/Scripts/Game.cs
+++ Sounds/Assets/Scripts/Game.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public AudioClipGroup CoinDropAudio;
+    public AudioClipGroup OwlAudio;
+    public AudioClipGroup FireAudio;
 
     public void Update()
     {
         if (Input.GetKey(KeyCode.Alpha1))
         {
             CoinDropAudio.Play();
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            OwlAudio.Play();
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            FireAudio.Play();
         }
     }
 }
