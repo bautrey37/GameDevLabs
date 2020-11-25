adc35b08f1e5f14087e1b247c4fc1bbcb1a220aa
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-26 01:41:26 +0200

    finish lab 12

---
+++ Lab12/Assets/Scripts/BillBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+
+    public void Awake()
+    {
+        cameraToLookAt = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v);
+    }
+}
