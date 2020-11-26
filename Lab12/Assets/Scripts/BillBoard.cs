b1fe0fe8481d70d57dd478bedf695d7a62653e45
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-11-26 03:10:19 +0200

    explode plays on trigger with player

--- Lab12/Assets/Scripts/BillBoard.cs
+++ Lab12/Assets/Scripts/BillBoard.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
-    public Camera cameraToLookAt;
+    public ParticleSystem Explosion;
+
+    private Camera cameraToLookAt;
+
+    private bool exploded = false;
+
+    Animator animator;
 
     public void Awake()
     {
         cameraToLookAt = Camera.main;
+    }
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -15,5 +26,21 @@ public class BillBoard : MonoBehaviour
         Vector3 v = cameraToLookAt.transform.position - transform.position;
         v.x = v.z = 0.0f;
         transform.LookAt(cameraToLookAt.transform.position - v);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CharacterController cont = other.GetComponent<CharacterController>();
+        if (!exploded && cont)
+        {
+            animator.SetTrigger("Explode");
+            Explode();
+        }
+        //exploded = true;
+    }
+
+    public void Explode()
+    {
+        Explosion.Play();
     }
 }
