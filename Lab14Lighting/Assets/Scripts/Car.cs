93e2aaebe1b9bee79304ab42bc4d56947c54a510
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-12-09 18:50:59 +0200

    finish lab 14

---
+++ Lab14Lighting/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private float speed = 3;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
